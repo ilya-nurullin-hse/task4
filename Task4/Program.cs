5343de16fcb7798b086a6574f4c1fd377776ae3d
Ilya Nurullin <nurullinin@mail.ru>
2017-07-24 20:52:18 +0500

    The first version of solution

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,33 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Введите положительное число: ");
+                try
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    int sqrtN = (int) Math.Sqrt(n);
+                    for (int i = 1; i <= sqrtN; i++)
+                    {
+                        for (int j = i; j <= sqrtN; j++)
+                        {
+                            int sum = sqrSum(i, j);
+                            if (sum <= n)
+                                Console.WriteLine(sum);
+                        }
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ожидалось положительное число");
+                }
+            }
+        }
+
+        static int sqrSum(int x, int y)
+        {
+            return x * x + y * y;
         }
     }
 }
