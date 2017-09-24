470cb98971e1040236c907d2841eefe2f9317953
Ilya Nurullin <nurullinin@mail.ru>
2017-09-25 00:09:59 +0500

    Add user input

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -11,6 +11,9 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(
+                "Дано натуральное число n. Среди чисел 1,2,...,n найти все те, которые можно представить в виде суммы квадратов двух " +
+                "натуральных чисел. (Определить процедуру, позволяющую распознавать полные квадраты).\n");
             while (true)
             {
                 Console.Write("Введите положительное число: ");
@@ -18,13 +21,17 @@ namespace Task4
                 {
                     int n = int.Parse(Console.ReadLine());
                     int sqrtN = (int) Math.Sqrt(n);
+                    var printed = new List<int>();
                     for (int i = 1; i <= sqrtN; i++)
                     {
                         for (int j = i; j <= sqrtN; j++)
                         {
                             int sum = sqrSum(i, j);
-                            if (sum <= n)
+                            if (sum <= n && !printed.Contains(sum))
+                            {
                                 Console.WriteLine(sum);
+                                printed.Add(sum);
+                            }
                         }
                     }
                 }
